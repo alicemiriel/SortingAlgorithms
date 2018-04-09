7f8d46324ee378bff6ecc30c377eda6798644316
alicemiriel <alicemiriel@gmail.com>
2018-04-09 20:15:48 +0200

    InsertionSorting

--- src/main/java/com/sda/sorting/Main.java
+++ src/main/java/com/sda/sorting/Main.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int [] table = {1,20,2,5,10,22};
-        CountingSort.sort(table);
+        int[] table = {1, 20, 2, 5, 10, 22, 15};
+        InsertionSort.sort(table);
         System.out.println(Arrays.toString(table));
 
 //        int [] table2 = {1,200, 3, 45,400, 2};
