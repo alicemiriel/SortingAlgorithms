708f47d9d8dd4919eb9ffa0e075e198cbc850500
alicemiriel <alicemiriel@gmail.com>
2018-04-09 20:16:34 +0200

    InsertionSorting

---
+++ src/main/java/com/sda/sorting/InsertionSort.java
@@ -0,0 +1,15 @@
+package com.sda.sorting;
+
+public class InsertionSort {
+    public static void sort(int[] tablica) {
+        for (int i = 0; i < tablica.length; i++) {
+            int key = tablica[i];
+            int j = i - 1;
+            while (j >= 0 && tablica[j] > key) {
+                tablica[j + 1] = tablica[j];
+                j = j - 1;
+                tablica[j + 1] = key;
+            }
+        }
+    }
+}
